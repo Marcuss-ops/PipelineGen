d7f9dd101b6c6bbcab466acf3b619b6464ae44ab
PierThatDev <futurimilionariposta@gmail.com>
2026-07-15 10:16:24 +0000

    feat(archcheck): register domain job baseline gate

--- cmd/archcheck/checks.go
+++ cmd/archcheck/checks.go
@@ -66,6 +66,9 @@ func DefaultChecks(productionOnly bool) []CheckSpec {
 		{"percheck_no_domain_job_compatibility_aliases", func(root string, pol *policy.Policy, r *report.Report) {
 			scan.ScanNoDomainJobCompatibilityAliases(root, pol, r, productionOnly)
 		}},
+		{"percheck_domain_job_import_baseline", func(root string, pol *policy.Policy, r *report.Report) {
+			scan.ScanDomainJobImportBaseline(root, pol, r, productionOnly)
+		}},
 		{"percheck_no_generic_generation_facade", func(root string, pol *policy.Policy, r *report.Report) {
 			scan.ScanNoGenericGenerationFacade(root, pol, r, productionOnly)
 		}},
