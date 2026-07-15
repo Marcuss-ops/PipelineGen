435317c36cca394707755491db4fb27c4709ebb4
PierThatDev <futurimilionariposta@gmail.com>
2026-07-15 10:11:30 +0000

    refactor(job-kernel): migrate texttracks materialize handler

--- internal/application/assets/texttracks/jobs.go
+++ internal/application/assets/texttracks/jobs.go
@@ -13,7 +13,7 @@ import (
 
 	appjobs "github.com/Marcuss-ops/PipelineGen/internal/application/jobs"
 	"github.com/Marcuss-ops/PipelineGen/internal/domain/asset"
-	job "github.com/Marcuss-ops/PipelineGen/internal/domain/job"
+	job "github.com/Marcuss-ops/PipelineGen/internal/kernel/job"
 	"go.uber.org/zap"
 )
 
